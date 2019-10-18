5d6673aae01a8d1525651b00255e5de3452debf0
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2019-10-18 11:20:32 +0200

    Try to use m.css theme

--- src/pelicanconf_mcss.py
+++ src/pelicanconf_mcss.py
@@ -9,10 +9,10 @@ import os
 import sys
 
 sys.path.append(os.curdir)
-from pelicanconf import *
+#  from pelicanconf import *
 
 THEME = "m.css/pelican-theme"
-THEME_STATIC_DIR = 'static'
+THEME_STATIC_DIR = "static"
 DIRECT_TEMPLATES = (
     "index",
     "tags",
@@ -25,16 +25,36 @@ DIRECT_TEMPLATES = (
 )
 
 M_CSS_FILES = [
-    'https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i%7CSource+Code+Pro:400,400i,600',
-    'theme/m-dark.css',
+    "https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,400i,600,600i%7CSource+Code+Pro:400,400i,600",
+    "/static/m-dark.css",
 ]
-M_THEME_COLOR = '#22272e'
+M_THEME_COLOR = "#22272e"
 
-PLUGIN_PATHS = ['../m.css/plugins']
-PLUGINS = ['m.htmlsanity']
+PLUGIN_PATHS = ["m.css/plugins"]
+PLUGINS = ["m.htmlsanity"]
 
 M_FAVICON = ("/images/KTH_logo.png", "image/png")
-M_BLOG_FAVICON = ("/images/KTH_logo.png", "image/png")
-M_SITE_LOGO = "/images/KTH_logo.png"
+#  M_BLOG_FAVICON = ("/images/KTH_logo.png", "image/png")
+#  M_SITE_LOGO = "/images/KTH_logo.png"
 M_SITE_LOGO_TEXT = "Ashwin Vishnu's Website"
 
+# Navbar
+M_LINKS_NAVBAR1 = [
+    (
+        "Posts", "archives", "[blog]",
+        [
+            ("Blog", "categories/blog.html", ""),
+            ("Tech Talk", "categories/tech-talk.html", ""),
+        ],
+    ),
+    ("Contact", "contact/", "contact", []),
+]
+
+M_LINKS_NAVBAR2 = [
+    ("Showcase", "showcase/", "[showcase]",
+        [
+            ("Research", "pages/research", ""),
+            ("Software", "pages/software", ""),
+        ]
+    ),
+]
