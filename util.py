38713f801df39c2db60a2da079b3875f967b7596
Ashwin Vishnu <ashwinvis+gh@protonmail.com>
2020-03-26 02:07:32 +0100

    Implement Planet

--- util.py
+++ util.py
@@ -1,5 +1,7 @@
 import codecs
 
+from defusedxml.ElementTree import parse
+
 
 def encrypt_email(real_name, rev_username, domain, tld="com"):
     """Encrypt email to avoid spam bots from scraping. Uses ROT13 encryption.
@@ -18,8 +20,25 @@ def encrypt_email(real_name, rev_username, domain, tld="com"):
     return email_rot13
 
 
+def read_opml(path):
+    tree = parse(path)
+    root = tree.getroot()
+
+    opml = {}
+    for child in root.find('body'):
+        category = child.get('title')
+        opml[category] = {}
+        for grandchild in child:
+            feed_title = grandchild.get('title')
+            feed_url = grandchild.get('xmlUrl')
+            opml[category][feed_title] = feed_url
+    return opml["Blogroll"]
+
+
 if __name__ == "__main__":
     # Generate email href
     AUTHOR = "Ashwin Vishnu Mohanan"
     print(encrypt_email(AUTHOR, rev_username="sivniwhsa", domain="pm", tld="me"))
     print(encrypt_email(AUTHOR, rev_username="omva", domain="misu.su", tld="se"))
+
+    read_opml("planet.opml")
