3b41637c76de54d7bd875996d8ac452f62e8fda3
ashwinvis <ashwinvis@users.noreply.github.com>
2020-04-24 06:27:05 +0000

    Automated deployment: Fri Apr 24 06:27:04 UTC 2020 052122c0896485ce5d902f72eba1754afcd314ca

---
+++ sw.js
@@ -0,0 +1,31 @@
+/* https://developer.mozilla.org/en-US/docs/Web/Progressive_web_apps/Offline_Service_workers
+ * */
+var cacheName = 'ashwin-info-tm-v1'
+var urlsToCache = [
+  '/',
+  '/index.html',
+  '/archives.html',
+  '/pages/research.html',
+  '/pages/software.html',
+  '/pages/cv.html',
+  '/pages/contact.html',
+  '/pages/planet.html',
+  '/static/custom.compiled.css',
+  '/static/m-dark.compiled.css',
+  'images/logo_ashwin.png',
+  'images/dp_ashwin_2016.jpg'
+]
+
+self.addEventListener('install', (event) => {
+  console.log('[Service Worker] Install')
+  event.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      console.log('[Service Worker] Caching all: app shell and content')
+      return cache.addAll(contentToCache)
+    })
+  )
+})
+
+self.addEventListener('fetch', (event) => {
+  console.log('[Service Worker] Fetched resource ' + event.request.url)
+})
